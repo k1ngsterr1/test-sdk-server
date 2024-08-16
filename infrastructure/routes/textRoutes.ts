bb72be7237290354c18273438f886dab7cecb090
Erlan-bam <erlanzh.gg@gmail.com>
2024-08-16 12:53:07 +0500

    --UPLOAD IMAGES

--- infrastructure/routes/textRoutes.ts
+++ infrastructure/routes/textRoutes.ts
@@ -2,9 +2,35 @@ import textController from "@presentation/controllers/textControllers";
 
 const express = require("express");
 
+const multer = require("multer");
+
 const router = express.Router();
 
-router.post("/add", (req, res) => textController.addText(req, res));
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const currentDate = new Date()
+      .toJSON("kz-kz")
+      .slice(0, 10)
+      .replace(/:/g, "-");
+    const hours = new Date().getHours().toString().padStart(2, "0");
+    const minutes = new Date().getHours().toString().padStart(2, "0");
+    const seconds = new Date().getSeconds().toString().padStart(2, "0");
+    const currentTime = `H=${hours}-M=${minutes}-S=${seconds}`;
+    let result =
+      currentDate.toString() + "-" + currentTime + "-" + file.originalname;
+    req.body.image = result;
+    cb(null, result);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/add", upload.single("image"), (req, res) =>
+  textController.addText(req, res)
+);
 
 router.get("/get-texts", (req, res) => textController.getTexts(req, res));
 
@@ -12,6 +38,8 @@ router.delete("/delete/:id/:code", (req, res) =>
   textController.deleteText(req, res)
 );
 
-router.patch("/update/:id", (req, res) => textController.updateText(req, res));
+router.patch("/update/:id", upload.single("image"), (req, res) =>
+  textController.updateText(req, res)
+);
 
 export default router;
